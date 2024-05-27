a987471f610dfc122a6ffaee79c7b09b5bfd2074
MartinEstefanell <martinestefanell@gmail.com>
2024-05-27 11:52:56 -0300

    canción

---
+++ uy.edu.um.prog2.adt/src/CSV/Song.java
@@ -0,0 +1,16 @@
+package CSV;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class Song {
+
+    public String spotify_id;
+    public String name;
+    public String artists;
+    public int daily_rank;
+    public String country;
+
+
+
+}

84c48ed7bd634faea1443ae7190668647ffd6b4e
MartinEstefanell <martinestefanell@gmail.com>
2024-05-28 17:11:28 -0300

    rama

--- uy.edu.um.prog2.adt/src/CSV/ManipularCSV.java
+++ uy.edu.um.prog2.adt/src/CSV/ManipularCSV.java
@@ -7,66 +7,32 @@ import javax.management.MBeanNotificationInfo;
 import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ManipularCSV {
-    private BufferedReader reader;
-    private String line;
-    private String parts[];
-    public String auxList[];
-    public BufferedReader getReader() {
-        return reader;
-    }
-
-
-
+    private List<String> lines = new ArrayList<>();
 
     public void readFile(String fileName){
-        try {
-            reader = new BufferedReader(new FileReader(fileName));
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+            String line;
             while ((line = reader.readLine()) != null){
-            parts = line.split(" ; ");
-            //printLine();
-            System.out.println();
+                lines.add(line);
             }
-            reader.close();
-            line = null;
-            parts = null;
-            } catch (Exception e) {
+        }catch (Exception e) {
                 JOptionPane.showMessageDialog(null, e);
-            }
-    }
-    public void opcion1(String pais, String dia) throws InvalidCountry {
-        for (String part : parts) {
-            for (int j = 0; j < 24; j++) {
-                auxList[j] = Arrays.toString(part.split(";"));
-            }
-            while (auxList[8] == dia) {
-                System.out.println(part);
-
-            }
-        }
-
-    }
-
-    public List<String[]> pais(List<String[]>list, int max){
-        String[] pais;
-        for (int i = 0; i<max; i++){
-            List =
         }
     }
-
-    public void Lista_Fechas(){
-        String fecha1;
-        String fecha2;
-        for()
+    public List<String> getLines(){
+        return lines;
     }
-
 
     public void printLine(){
-        for (int i=0; i<parts.length;i++){
-            System.out.println(parts[i] + "  |  ");
+        for (int i=0; i<lines.size();i++){
+            System.out.println(lines.get(i) + "  |  ");
+            System.out.println();
         }
     }
 
--- uy.edu.um.prog2.adt/src/CSV/pruebaCSV.java
+++ uy.edu.um.prog2.adt/src/CSV/pruebaCSV.java
@@ -1,14 +1,24 @@
 package CSV;
 
 import CSV.Exceptions.InvalidCountry;
+import TADs.Hash.Exceptions.InvalidHashKey;
 
+import java.io.FileNotFoundException;
 import java.util.Date;
 
+import static CSV.MisMetodos.opcion1;
+
 public class pruebaCSV {
-    public static void main(String[] args) throws InvalidCountry {
-        ManipularCSV file = new ManipularCSV();
-        file.readFile("C:\\Users\\Usuario\\Desktop\\Obligatorio Exceel.csv");
-        file.opcion1("ZA", "13-5-2024");
+    public static void main(String[] args) throws InvalidCountry, FileNotFoundException {
+        try {
+            ManipularCSV file = new ManipularCSV();
+            Metodos metodos = new Metodos();
+            file.readFile("C:\\Users\\Usuario\\Desktop\\Obligatorio Exceel.csv");
+            //file.printLine();
+            metodos.opcion1("ZA", "13-5-2024", file);
+        } catch (InvalidCountry e) {
+            System.err.println(e.getMessage());
+        }
 
     }
 
